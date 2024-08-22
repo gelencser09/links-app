f88528326ec32b730e65eb1203b8d2d9daa3d2a2
Ákos Gelencsér <gelencser09@gmail.com>
2024-08-22 11:31:57 +0200

    fix: layout

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -115,7 +115,7 @@ export const Logo = () => (
 
 export const Logout = () => (
   <svg
-    className="w-6 h-6 text-gray-800 dark:text-white"
+    className="w-3 h-3 text-gray-800 dark:text-white"
     aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
     width="24"
--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -10,7 +10,10 @@ export function LogoutButton() {
       type="button"
       className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm p-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
     >
-      <Logout />
+      <div className="flex items-center">
+        <div className="me-2">Log out </div>
+        <Logout />
+      </div>
     </button>
   );
 }
