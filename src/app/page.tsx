7043b26f8b1936d772ed1d2146c110b0bf07a2b2
Ákos Gelencsér <gelencser09@gmail.com>
2024-08-22 12:00:41 +0200

    feat: add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,58 @@
+import { Card, CardBody, CardHeader } from "@/components/card";
 import { fetchUsers } from "./lib/users-data";
 
 export default async function Home() {
   const allUsers = await fetchUsers();
   return (
     <>
-      {allUsers.map((user) => (
-        <div key={user.id}>{user.username}</div>
-      ))}
+      <Card>
+        <CardHeader>Welcome!</CardHeader>
+        <div
+          className="flex items-center p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800"
+          role="alert"
+        >
+          <svg
+            className="flex-shrink-0 inline w-4 h-4 me-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+          </svg>
+          <span className="sr-only">Info</span>
+          <div>
+            This is only a prototype/demo app and is not meant for production
+            use. Do not provide any personal information.
+          </div>
+        </div>
+        <CardBody>
+          <ul className="list-disc list-inside">
+            <li className="mt-5 text-center">
+              Feel free to try this app, by{" "}
+              <a href="/auth/register" className="underline">
+                creating a user
+              </a>{" "}
+              and{" "}
+              <a href="/auth/login" className="underline">
+                logging in
+              </a>
+              .
+            </li>
+            <li className="mt-5 text-center">
+              You can{" "}
+              <a href="/me" className="underline">
+                manage your links
+              </a>{" "}
+              after logging in and clicking on your user next to the logout
+              button.
+            </li>
+            <li className="mt-5 text-center">
+              See other users and share your links at /user/{"{username}"}
+            </li>
+          </ul>
+        </CardBody>
+      </Card>
     </>
   );
 }
